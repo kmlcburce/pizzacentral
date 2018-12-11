e0480c7c94f4b1428552cf94d29af2bf23737e9a
DigitalCielo <ridenzel34@gmail.com>
2018-12-11 15:52:14 +0800

    added fcrud and edited admin.php

---
+++ application/views/admin/fcrud.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<?php
+        
+                $conn = mysqli_connect("localhost","root","","pizzacentral");
+                if(!$conn){
+                        die('Not connected to Database'.mysqli_connect_error());
+                }else{
+
+                        $price = mysqli_query($conn,"SELECT * FROM price_list LEFT JOIN product_profile product_profile.prod_id = price_list.prod_id");
+                        if(mysqli_num_rows($price) > 0){
+                        
+                        while($row = mysqli_fetch_array($price)){
+                               
+                        ?>
+		<form>
+			<table>
+				<tr>
+					<td>ID</td><td>Name</td><td>Description</td><td>Price</td><td>Status</td>
+				</tr>
+				<tr>
+					
+				</tr>
+			</table>
+		</form>
+</body>
+</html>
