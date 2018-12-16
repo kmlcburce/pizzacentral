3bd5a62ae77695b663b2ebafa9f2b01d3081914e
CpE Chair <kmlcburce@gmail.com>
2018-12-16 10:33:46 +0800

    updated the database file

--- pizzacentral.sql
+++ pizzacentral.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2018 at 06:54 AM
+-- Generation Time: Dec 16, 2018 at 03:33 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -44,10 +44,18 @@ CREATE TABLE `customer` (
 
 CREATE TABLE `order_form` (
   `order_id` int(10) UNSIGNED ZEROFILL NOT NULL,
-  `cust_id` int(10) NOT NULL,
-  `prod_id` int(10) NOT NULL,
+  `cust_id` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `prod_id` int(10) UNSIGNED ZEROFILL NOT NULL,
   `order_refn` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_form`
+--
+
+INSERT INTO `order_form` (`order_id`, `cust_id`, `prod_id`, `order_refn`) VALUES
+(0000000001, 0000000001, 0000000001, 1),
+(0000000002, 0000000002, 0000000003, 2);
 
 -- --------------------------------------------------------
 
@@ -56,7 +64,7 @@ CREATE TABLE `order_form` (
 --
 
 CREATE TABLE `product_profile` (
-  `prod_id` int(10) NOT NULL,
+  `prod_id` int(10) UNSIGNED ZEROFILL NOT NULL,
   `prod_name` varchar(100) NOT NULL,
   `prod_type` varchar(10) NOT NULL,
   `prod_desc` varchar(100) NOT NULL,
@@ -69,10 +77,10 @@ CREATE TABLE `product_profile` (
 --
 
 INSERT INTO `product_profile` (`prod_id`, `prod_name`, `prod_type`, `prod_desc`, `prod_imgurl`, `prod_price`) VALUES
-(1, 'Margherita', 'pizza', 'Made with San Marzano tomatoes, mozzarella fior di latte, fresh basil, salt and extra-virgin olive o', 'assets/images/menu-1-120x120.jpg', 299),
-(2, 'Hawaiian', 'Pizza', 'Pineapple, Tomato sauce, Ham slices', 'assets/images/menu-1-120x120.jpg', 250),
-(3, 'Napolitan', 'pasta', 'tomato sauce, basil', 'assets/images/menu-1-120x120.jpg', 150),
-(4, 'Agli o olio', 'pasta', 'Olive oil, basil, pasta', 'assets/images/menu-1-120x120.jpg', 260);
+(0000000001, 'Margherita', 'pizza', 'Made with San Marzano tomatoes, mozzarella fior di latte, fresh basil, salt and extra-virgin olive o', 'assets/images/menu-1-120x120.jpg', 299),
+(0000000002, 'Hawaiian', 'Pizza', 'Pineapple, Tomato sauce, Ham slices', 'assets/images/menu-1-120x120.jpg', 250),
+(0000000003, 'Napolitan', 'pasta', 'tomato sauce, basil', 'assets/images/menu-1-120x120.jpg', 150),
+(0000000004, 'Agli o olio', 'pasta', 'Olive oil, basil, pasta', 'assets/images/menu-1-120x120.jpg', 260);
 
 -- --------------------------------------------------------
 
@@ -102,7 +110,9 @@ ALTER TABLE `customer`
 -- Indexes for table `order_form`
 --
 ALTER TABLE `order_form`
-  ADD PRIMARY KEY (`order_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `cust_id` (`cust_id`),
+  ADD KEY `prod_id` (`prod_id`);
 
 --
 -- Indexes for table `product_profile`
@@ -130,19 +140,35 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `order_form`
 --
 ALTER TABLE `order_form`
-  MODIFY `order_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `product_profile`
 --
 ALTER TABLE `product_profile`
-  MODIFY `prod_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `prod_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `u_id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`cust_id`) REFERENCES `order_form` (`cust_id`);
+
+--
+-- Constraints for table `order_form`
+--
+ALTER TABLE `order_form`
+  ADD CONSTRAINT `order_form_ibfk_1` FOREIGN KEY (`prod_id`) REFERENCES `product_profile` (`prod_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
